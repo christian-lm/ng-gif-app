86ef06d5fc52361568b7514456929dcfb9c520f2
Christian Lozano <christian.lozano@neoris.com>
2022-10-10 17:22:13 +0200

    - Creación deL modulo de 'gifs' y de los componentes para la pagina principal, la 'busqueda' y 'resultados'

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html'
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
