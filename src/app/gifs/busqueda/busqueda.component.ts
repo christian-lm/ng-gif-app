e7283939e5ea55999d3d81a5fae44b7b50dc99af
Christian Lozano <christian.lozano@neoris.com>
2022-10-11 09:32:32 +0200

    - Se crea el metodo para buscar gif con la caja de texto y que se borre el valor tras pulsar 'Enter'

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html'
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() {
+  // -- Buscar el elemento HTML con la referencia. Tambien sirve con elementos HTML (input, h3...) --
+  // -- El '!' explica que nunca va a ser nulo este campo --
+  // -- El HTMLInputElement es para decirle al ElementRef que es un Input --
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  /**
+   * Método buscar GIFS
+   */
+  buscar() {
+    const value = this.txtBuscar.nativeElement.value;
+    console.log(value);
+    this.txtBuscar.nativeElement.value = '';
   }
-
-  ngOnInit(): void {
-  }
-
 }
